2aeeb6e14709b363cc8a70cd52b1063fcd3a60b9
zhuseynov <zaur1991@gmail.com>
2022-09-11 23:25:19 +0400

    MainMenu

--- Welcome.py
+++ Welcome.py
@@ -1,12 +1,12 @@
 import streamlit as st
 st.set_page_config(layout='wide')
-hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        </style>
-        """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
+# hide_menu_style = """
+#         <style>
+#         #MainMenu {visibility: hidden;}
+#         footer {visibility: hidden;}
+#         </style>
+#         """
+# st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 from PIL import Image
 
